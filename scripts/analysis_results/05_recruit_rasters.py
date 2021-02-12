6dd919297dd6c27cf5a543fd600cde9811021c81
jcristiani <jcristia10@gmail.com>
2021-02-11 22:53:12 -0800

    no message

--- scripts/analysis_results/05_recruit_rasters.py
+++
@@ -1,4 +0,0 @@
-# It still makes sense to use just the 60 day pld destination points for chapter 4
-# It will show all points that strand.
-# In a way it is already a combined version of all the PLDs before it.
-# I can present it as the accumulation of all settlement.
