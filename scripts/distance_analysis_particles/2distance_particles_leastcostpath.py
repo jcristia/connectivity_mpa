0ac74d3b9b4bd3a3109d78f97916691b1b5b5474
jcristiani <jcristia10@gmail.com>
2021-09-01 12:24:05 -0700

    convert features to layers before processing

--- scripts/distance_analysis_particles/2distance_particles_leastcostpath.py
+++ scripts/distance_analysis_particles/2distance_particles_leastcostpath.py
@@ -3,6 +3,7 @@
 
 import arcpy
 import pandas as pd
+import numpy as np
 
 
 prep_gdb = r'C:\Users\jcristia\Documents\GIS\MSc_Projects\MPA_connectivity\scripts\distance_analysis_particles\spatial\dist_dataprep.gdb'
@@ -44,11 +45,33 @@ for fc in fcs_dest:
 
 origin_list = df_combos.uID_ref_origin.unique()
 cent_orig = os.path.join(prep_gdb, 'CENTROIDS_origin')
+arcpy.MakeFeatureLayer_management(cent_orig, 'cent_orig_lyr')
+# making feature layers before doing the select by attribute made all the
+# difference for speed. It was so slow before. SelectLayerByAttribute says that
+# it creates a layer before doing a select which makes you think that making
+# a feature layer before is unecessary, but it made a ton of difference doing it
+# yourself.
 cent_dest = os.path.join(prep_gdb, 'CENTROIDS_dest')
+arcpy.MakeFeatureLayer_management(cent_dest, 'cent_dest_lyr')
 land_r_500 = os.path.join(prep_gdb, 'land_r_500')
 arcpy.env.snapRaster = land_r_500
 arcpy.env.extent = land_r_500
 arcpy.env.workspace = euclines_gdb
+
+# if you need to start this loop from a different value:
+value = 8829
+index_v = np.where(origin_list == value)[0][0]
+# otherwise, just index it as [:]
+origin_list = origin_list[index_v:]
+origin_list
+
+# When running this, some points would return an error. There were some where,
+# for whatever reason, the cell still overlapped with land, and so I manually
+# moved the origin point. Others just didn't return any lines for some reason
+# even though there wasn't any overlap. I moved these one cell over and then it
+# was fine. So, some distances are going to be 500 meters off. That's just how
+# it is going to be.
+
 for origin_pt in origin_list:
 
     print(f'processing origin point {origin_pt}')
@@ -57,11 +80,14 @@ for origin_pt in origin_list:
     dest_list = list(df_combos[df_combos.uID_ref_origin==origin_pt].uID_ref_dest.unique())
 
     # from CENTROIDS_origin, select that point and convert to raster
-    centroid_sel = arcpy.SelectLayerByAttribute_management(cent_orig, 'NEW_SELECTION', f'uID_ref_origin={origin_pt}')
+    print('selecting origin point')
+    centroid_sel = arcpy.SelectLayerByAttribute_management('cent_orig_lyr', 'NEW_SELECTION', f'uID_ref_origin={origin_pt}')
     origin = 'centroid_origin_{}'.format(str(origin_pt))
+    print('rasterizing origin point')
     arcpy.FeatureToRaster_conversion(centroid_sel, 'uID_ref_origin', origin, 500)       
 
     # Euclidean distance with barriers
+    print('creating Euclidean distance raster')
     in_rast = origin
     max_dist = ''
     cellsize = land_r_500
@@ -74,9 +100,12 @@ for origin_pt in origin_list:
 
     # Cost Path as Polyline
     # create destination layer, select from CENTROIDS_dest with dest_list
-    cent_dest_sel = arcpy.SelectLayerByAttribute_management(cent_dest, 'NEW_SELECTION', f'"uID_ref_dest" IN {str(tuple(dest_list))}')
+    print('selecting destination points')
+    cent_dest_sel = arcpy.SelectLayerByAttribute_management('cent_dest_lyr', 'NEW_SELECTION', f'"uID_ref_dest" IN {str(tuple(dest_list))}')
     dest = f'centroid_dest_{origin_pt}'
+    print('rasterizing destination points')
     arcpy.FeatureToRaster_conversion(cent_dest_sel, 'uID_ref_dest', dest, 500)
+    print('creating cost path lines')
     inputDestinationLayer = dest
     inputCostLayer = 'eucdistance'
     inputDirectionLayer = 'eucbackdirect'
@@ -86,12 +115,15 @@ for origin_pt in origin_list:
     arcpy.sa.CostPathAsPolyline(inputDestinationLayer, inputCostLayer, inputDirectionLayer, outLines, pathType, destfield)
 
     # add origin_uid attribute
+    print('adding field')
     arcpy.AddField_management(outLines, 'origin_id', 'SHORT')
+    print('calculating field')
     with arcpy.da.UpdateCursor(outLines, ['origin_id']) as cursor:
         for row in cursor:
             row[0] = int(origin_pt)
             cursor.updateRow(row)
 
+    print('deleting data')
     arcpy.Delete_management(origin)
     arcpy.Delete_management('eucbackdirect')
     arcpy.Delete_management('eucdistance')
